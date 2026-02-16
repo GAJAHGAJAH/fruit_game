2ce8effba1e619a6256e6038e5ac56d2f09fa4fa
GAJAHGAJAH <1123150165@global.ac.id>
2026-02-16 15:26:15 +0700

    update fruit catcher

---
+++ lib/core.dart
@@ -0,0 +1,11 @@
+/*
+We believe, the class name must be unique. 
+If there is a conflicting class name in this file,
+it means you have to rename it to something more unique.
+fileCount: 5
+*/
+export 'package:game_fruit/core_package.dart';
+export 'package:game_fruit/game/components/basket.dart';
+export 'package:game_fruit/game/components/fruit.dart';
+export 'package:game_fruit/game/fruit_catcher_game.dart';
+export 'package:game_fruit/game/managers/audio_manager.dart';
--- lib/game/fruit_catcher_game.dart
+++ lib/game/fruit_catcher_game.dart
@@ -1,13 +1,100 @@
+import 'dart:math';
+import 'package:flame/camera.dart';
+import 'package:flame/components.dart';
+import 'package:flame/events.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
-class FruitCatcherGame extends FlameGame {
+import 'components/basket.dart';
+import 'components/fruit.dart';
+import 'managers/audio_manager.dart';
+
+class FruitCatcherGame extends FlameGame
+    with PanDetector, HasCollisionDetection {
+
+  late Basket basket;
+  late TextComponent scoreText;
+
+  final Random random = Random();
+
+  double fruitSpawnTimer = 0;
+  final double fruitSpawnInterval = 1.5;
+
   final ValueNotifier<int> scoreNotifier = ValueNotifier<int>(0);
 
+  int _score = 0;
+
+  int get score => _score;
+
+  set score(int value) {
+    _score = value;
+    scoreNotifier.value = value;
+  }
+
   @override
-  Color backgroundColor() => const Color(0xFF87CEEB);
+  Future<void> onLoad() async {
+    await super.onLoad();
+
+    // Set fixed resolution viewport
+    camera.viewport =
+        FixedResolutionViewport(resolution: Vector2(400, 800));
+
+    // Add basket
+    basket = Basket();
+    await add(basket);
+
+    // Play background music
+    AudioManager().playBackgroundMusic();
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+
+    // Spawn fruits setiap interval
+    fruitSpawnTimer += dt;
+
+    if (fruitSpawnTimer >= fruitSpawnInterval) {
+      spawnFruit();
+      fruitSpawnTimer = 0;
+    }
+  }
+
+  void spawnFruit() {
+    final x = random.nextDouble() * size.x;
+    final fruit = Fruit(position: Vector2(x, -50));
+    add(fruit);
+  }
+
+  @override
+  void onPanUpdate(DragUpdateInfo info) {
+    // Geser basket mengikuti drag
+    basket.position.x += info.delta.global.x;
+
+    // Batasi agar tidak keluar layar
+    basket.position.x = basket.position.x.clamp(
+      basket.size.x / 2,
+      size.x - basket.size.x / 2,
+    );
+  }
 
   void incrementScore() {
-    scoreNotifier.value++;
+    score++;
+    AudioManager().playSfx('collect.mp3');
   }
+
+  void gameOver() {
+    AudioManager().playSfx('explosion.mp3');
+    pauseEngine();
+    // Bisa tambahkan dialog Game Over di sini
+  }
+
+  @override
+  void onRemove() {
+    AudioManager().stopBackgroundMusic();
+    super.onRemove();
+  }
+
+  @override
+  Color backgroundColor() => const Color(0xFF87CEEB); // Sky blue
 }
