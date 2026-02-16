2ab9a0bf504cf5ab28982769674fee9d58e22f88
GAJAHGAJAH <1123150165@global.ac.id>
2026-02-16 13:37:13 +0700

    menambahkan file fruit catcher

---
+++ lib/game/fruit_catcher_game.dart
@@ -0,0 +1,13 @@
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+
+class FruitCatcherGame extends FlameGame {
+  final ValueNotifier<int> scoreNotifier = ValueNotifier<int>(0);
+
+  @override
+  Color backgroundColor() => const Color(0xFF87CEEB);
+
+  void incrementScore() {
+    scoreNotifier.value++;
+  }
+}
