691c1bada5b9f2c03dabaedcdc0c2f6dce85bff2
GAJAHGAJAH <1123150165@global.ac.id>
2026-02-16 13:33:34 +0700

    meanambahkan icon kontrol musik dan suara

--- lib/main.dart
+++ lib/main.dart
@@ -8,22 +8,21 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'fruit catcher game',
-      debugShowCheckedModeBanner: false,
-      home: const GameScreen(),
+    return const MaterialApp(
+      title: 'Fruit Catcher Game',
+      home: GameScreen(),
     );
   }
 }
 
 class GameScreen extends StatelessWidget {
+  const GameScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Center(
         child: Text('Game akan ditampilkan di sini'),
       ),
@@ -37,29 +36,63 @@ class GameScreen extends StatefulWidget {
   @override
   State<GameScreen> createState() => _GameScreenState();
 }
+
 class _GameScreenState extends State<GameScreen> {
-  @override
-  void initState() {
-    super.initState();
-  }
+
+  final ValueNotifier<int> counter = ValueNotifier(0);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
           Positioned(
+  top: 50,
+  right: 20,
+  child: Row(
+    children: [
+      IconButton(
+        icon: const Icon(Icons.music_note),
+        onPressed: () {},
+      ),
+      IconButton(
+        icon: const Icon(Icons.volume_up),
+        onPressed: () {},
+      ),
+    ],
+  ),
+),
+          Positioned(
             top: 50,
-            left: 290,
+            left: 20,
             child: Container(
-              padding: const EdgeInsets.all(10.0),
+              padding: const EdgeInsets.all(10),
               decoration: BoxDecoration(
                 color: Colors.black54,
-                borderRadius: BorderRadius.circular(10.0),
+                borderRadius: BorderRadius.circular(10),
               ),
-            )
+              child: ValueListenableBuilder<int>(
+                valueListenable: counter,
+                builder: (context, score, child) {
+                  return Text(
+                    'Score: $score',
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  );
+                },
+              ),
+            ),
           ),
         ],
       ),
     );
   }
 }
+
+
+
+
+
